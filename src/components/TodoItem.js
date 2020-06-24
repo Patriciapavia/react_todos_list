9555448549a766209c643d7b2503f6a31b6f1d61
patricia <patriciapavia236@gmail.com>
2020-06-24 18:30:52 +1000

    add fuctionality to toggle complete item

--- src/components/TodoItem.js
+++ src/components/TodoItem.js
@@ -2,10 +2,29 @@ import React, { Component } from "react";
 import PropTypes from "prop-types";
 
 export class TodoItem extends Component {
+  getStyle = () => {
+    return {
+      background: "#f4f4f4",
+      paddin: "10px",
+      borderBottom: "1px #ccc dotted",
+      textDecoration: this.props.todo.completed ? "line-through" : "none",
+    };
+  };
+
   render() {
+    const { id, title } = this.props.todo;
     return (
-      <div>
-        <p>{this.props.todo.title}</p>
+      <div style={this.getStyle()}>
+        <p>
+          <input
+            type="checkbox"
+            onChange={this.props.markComplete.bind(this, id)}
+          ></input>
+          {title}
+
+          {/* <input type="checkbox" onChange={this.props.markComplete.bind(this, this.props.todo.id)}>
+          {this.props.todo.title}</input> */}
+        </p>
       </div>
     );
   }
